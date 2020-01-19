e460ac0b910b1180293fc2ccec80c0047a3e2bab
Akash Jeganath <akashjoffl@icloud.com>
2020-01-19 23:14:33 +0530

    Constructor

--- javaScript.js
+++ javaScript.js
@@ -1,6 +1,14 @@
-let movie = {
-    title = "Inception",
-    director = "Christopher Nolan",
-    runtime = 69,
-    watched = true
-};
+class Movie {
+    constructor (title, director, runtime, watched) {
+        this.title = title;
+        this.director = director;
+        this.runtime = runtime;
+        this.watched = watched;
+    }
+}
+
+let firstMovie = new Movie ("Inception", "Nolan", 69, true);
+let secondMovie = new Movie ("STM", "Rajiv Menon", 140, false);
+
+console.log(firstMovie);
+console.log(secondMovie);
