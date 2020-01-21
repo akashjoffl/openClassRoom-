d4b045926e3b6ba1112cc5dbc68bc0483d51a51d
Akash Jeganath <akashjoffl@icloud.com>
2020-01-21 23:41:48 +0530

    The for…in loop

--- javaScript.js
+++ javaScript.js
@@ -1,33 +1,10 @@
-// const passengers = [
-//     'Will Alexander',
-//     'Sarah Kate',
-//     'Audrey Simon',
-//     'Tau Perkington'
-// ]
-
-// for (let passenger of passengers) {
-//     console.log('Boarding passenger ' + passenger);
-// }
-
 const passengers = [
-    {
-        name: 'Will Alexander',
-        ticketNumber: 209542
-    },
-    {
-        name: 'Sarah Kate',
-        ticketNumber: 169336
-    },
-    {
-        name: 'Audrey Simon',
-        ticketNumber: 779042
-    },
-    {
-        name: 'Tau Perkington',
-        ticketNumber: 703911
-    }
+    'Will Alexander',
+    'Sarah Kate',
+    'Audrey Simon',
+    'Tau Perkington'
 ]
 
-for (let passenger of passengers) {
-    console.log('Boarding passenger ' + passenger.name + ' with ticket number ' + passenger.ticketNumber);
+for (let i in passengers) {
+    console.log('Boarding passenger ' + passengers[i]);
 }
