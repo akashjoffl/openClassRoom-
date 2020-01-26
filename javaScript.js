9a2db9ce3ab59f1ff2c49740fb4609db6a18875a
Akash Jeganath <akashjoffl@icloud.com>
2020-01-26 23:02:03 +0530

    added comments

--- javaScript.js
+++ javaScript.js
@@ -1,22 +1,31 @@
-import { tau, meldrum, clara } from './database.js';
-
-const createShowComponents = (show) => {
-	const titleText = show.title;
-	const seasonsText = show.numberOfSeasons + ' seasons';
-	const episodesText = show.episodesPerSeason + ' episodes per season';
-	const Component = {
-		titleText: titleText,
-		seasonsText: seasonsText,
-		episodesText: episodesText
-    };
-    return Component;
-};
-
-const tauComponent = createShowComponents(tau);
-const meldrumComponent = createShowComponents(meldrum);
-const claraComponent = createShowComponents(clara);
-
-
-const showComponents = [ tauComponent, meldrumComponent, claraComponent ];
-
-export { showComponents };
+const getWordCount = (stringToTest) => {
+    const wordArray = stringToTest.split(' ');
+    return wordArray.length;
+  }
+  
+  const getLetterCount = (stringToTest) => {
+    const wordArray = stringToTest.split(' ');
+    let totalLetters = 0;
+    for (let word of wordArray) {
+      // remove punctuation so as to only count letters
+      word.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "");
+      totalLetters += word.length;
+    }
+    return totalLetters;
+  }
+  
+  /*
+  ** returns average word length to 
+  ** two decimal places
+  */
+  const getAverageWordLength = (stringToTest) => {
+    return parseFloat((getLetterCount(stringToTest) / getWordCount(stringToTest)).toFixed(2));
+  }
+  
+  const printStringStats = (stringToTest) => {
+    console.log({
+      wordCount: getWordCount(stringToTest),
+      letterCount: getLetterCount(stringToTest),
+      averageWordLength: getAverageWordLength(stringToTest)
+    })
+  }
